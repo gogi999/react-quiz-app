a8d6d0dd2dfb24fc164dd9122082224e12901574
gogi999 <g.petrovic9@gmail.com>
2023-11-29 11:50:34 +0100

    Added Header, Quiz and QuestionTimer components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import quizLogo from '../assets/quiz-logo.png';
+
+function Header() {
+  return (
+    <header>
+      <img src={quizLogo} alt="Quiz logo" />
+      <h1>React Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,27 @@
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+
+function QuestionTimer({ timeout, onTimeout }) {
+    const [remainingTime, setRemainingTime] = useState(timeout);
+
+    useEffect(() => {
+        console.log('SETTING TIMEOUT');
+        setTimeout(onTimeout, timeout);
+    }, [timeout, onTimeout]);
+    
+
+    useEffect(() => {
+        console.log('SETTING INTERVAL');
+        setInterval(() => {
+            setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+        }, 100);
+    }, []);
+
+    return (
+        <progress id="question-time" max={timeout} value={remainingTime} />
+    );
+}
+
+export default QuestionTimer
